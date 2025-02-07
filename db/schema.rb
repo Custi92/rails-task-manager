1a370e25b86e46dd3d1ba16eb16d39d8c526e4f4
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-02-07 10:56:35 +0100

    add completed to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_07_094936) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_07_095424) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
