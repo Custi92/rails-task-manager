fc00a2e51a23d226fa42e11c5c2c43fdd350a66f
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-02-12 14:58:39 +0100

    Rails Task Manager Done

--- db/migrate/20250207095424_add_completed_to_tasks.rb
+++ db/migrate/20250207095424_add_completed_to_tasks.rb
@@ -1,4 +1,5 @@
 class AddCompletedToTasks < ActiveRecord::Migration[7.1]
   def change
+    add_column :tasks, :completed, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_07_095424) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_07_112017) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false, null: false
   end
 
 end
